7db590ca8a3f39f868e04b060b96e62500432f9c
Mertaag <141136569+Mertaag@users.noreply.github.com>
2024-07-31 15:37:25 -0400

    Done 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+function findTheOldest(people) {
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    if (oldestAge < currentAge) {
+      return currentPerson
+    } else {
+      return oldest
+    }
+  })
+}
 
-};
+function getAge(birth, death){
+  if(!death){
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
